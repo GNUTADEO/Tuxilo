155c37a6476d81e7f4d0d59ac72bd71a29353e2c
Ludwig <theludway@gmail.com>
2025-12-14 04:25:55 +0000

    Cambios para el LXC

--- App/Front/front/vite.config.ts
+++ App/Front/front/vite.config.ts
@@ -3,5 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+plugins: [tailwindcss(), sveltekit()],
+server: {
+host: true,
+allowedHosts: [
+'tuxhydro.ludwigalvarado.me',
+'localhost',
+'192.168.1.5'
+]
+}
 });
