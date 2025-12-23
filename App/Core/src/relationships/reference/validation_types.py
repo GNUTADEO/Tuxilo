d4095d6b9c226236d8e8a2e9ac67508c2f236818
SpanishSyntax <SpanishSyntax@cuspide.club>
2025-12-23 13:03:47 -0500

    testeo deploy

--- App/Core/src/relationships/reference/validation_types.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Mapped, relationship
-from models import FieldRule
-
-field_rule: Mapped["FieldRule"] = relationship(
-    "FieldRule", back_populates="rule", uselist=False
-)
--- App/Deploy-2/pusher.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker push spanishhans/apps:BLOG-Persister
-docker push spanishhans/apps:BLOG-Blog
